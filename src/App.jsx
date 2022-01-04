ea86368b65b96c6742590130de4c19cc3c4ca750
Julio Fuentes <juliodddddd1234@gmail.com>
2022-01-04 15:53:03 -0600

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-	return <div className="bg-red-500">Socando para que funcione el deploy</div>;
+	return (
+		<div className="bg-red-500">Socando para que se actualize viteeee</div>
+	);
 }
 
 export default App;
