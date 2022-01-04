5b2a8cffa536ef0e1288e54919681614618c6f30
Julio Fuentes <juliodddddd1234@gmail.com>
2022-01-04 15:30:22 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	return <div className="bg-red-500">Socando para que funcione el deploy</div>;
+}
+
+export default App;
