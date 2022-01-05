63e14112ac07c2f96a842e3f5571815625e3c1e8
Julio Fuentes <juliodddddd1234@gmail.com>
2022-01-05 15:38:12 -0600

    Title square has been created

---
+++ src/components/TitleSquare.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TitleSquare = () => {
+	return (
+		<div className="customBackground w-72 h-48 flex flex-col justify-end">
+			<span className="font-bold text-white text-2xl mx-5 my-1">
+				Julio Fuentes
+			</span>
+			<span className="text-gray-300 text-xl mx-5 mb-6">Feedback Board</span>
+		</div>
+	);
+};
+
+export default TitleSquare;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/FeedbackCard";
+import App from "./components/TitleSquare";
 
 ReactDOM.render(
 	<React.StrictMode>
