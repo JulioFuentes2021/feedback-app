fabdc168e2f8319ddde3f107dcd6bcca8702464a
Julio Fuentes <juliodddddd1234@gmail.com>
2022-01-05 15:30:11 -0600

    Feedback cards are created

---
+++ src/components/FeedbackCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { FaComment, FaChevronUp } from "react-icons/fa";
+
+const FeedbackCard = () => {
+	return (
+		<article className="flex items-end sm:items-center p-4">
+			<section className="flex flex-col-reverse sm:flex-row">
+				<div className="sm:mx-6 bg-gray-200 w-20 h-10 rounded-xl flex sm:flex-col sm:w-12 sm:h-16 items-center justify-center">
+					<span className="text-blue-600 text-center mx-1">
+						<FaChevronUp />
+					</span>
+					<span className="font-semibold mx-1">112</span>
+				</div>
+				<div className="flex flex-col">
+					<span className="font-semibold text-xl">Add tags for solution</span>
+					<span className="my-2 text-gray-600">
+						Easier to watch for solutions based on a specific stack.
+					</span>
+					<button className="rounded-xl mb-2 bg-gray-200 p-2 w-36 font-semibold text-blue-600">
+						Enhacement
+					</button>
+				</div>
+			</section>
+			<section className="flex items-center absolute right-0 mx-4">
+				<span className="mx-2 text-gray-300 text-2xl">
+					<FaComment />
+				</span>
+				<span className="font-semibold">2</span>
+			</section>
+		</article>
+	);
+};
+
+export default FeedbackCard;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/AddFeedback";
+import App from "./components/FeedbackCard";
 
 ReactDOM.render(
 	<React.StrictMode>
